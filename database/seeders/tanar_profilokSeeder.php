9d9b6233d652053a8c6db7fa9f254edd7c9b2ae6
Schmidt Attila <schmidtattila@EOTVOS2.LOCAL>
2023-09-05 20:09:09 +0200

    mintaprofilok-tanar

--- database/seeders/tanar_profilokSeeder.php
+++ database/seeders/tanar_profilokSeeder.php
@@ -14,7 +14,7 @@ class tanar_profilokSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('tanar_profilok')->insert([
+        $tanarok = [[
             'nev' => 'Gipsz Jakab',
             'leiras' => '10 éve foglalkozok énekléssel',
             'irsz' => 1007,
@@ -23,7 +23,83 @@ class tanar_profilokSeeder extends Seeder
             'telefonszam' => 06012345678,
             'email' => 'teszt@teszt.com',
             'oradij' => 5000,
-            'elofizetes_lejarata' => '2024-07-01',
-        ]);
+            'elofizetes_lejarata' => '2024-07-01'
+        ],
+        [
+            'nev' => 'Kovács Béla',
+            'leiras' => '10 éve foglalkozok zongorázással',
+            'irsz' => 1007,
+            'telepules' => 'Budapest',
+            'cim' => 'Petőfi Sándor utca 2',
+            'telefonszam' => 06012345678,
+            'email' => 'teszt@teszt.com',
+            'oradij' => 5000,
+            'elofizetes_lejarata' => '2024-05-01'
+        ],
+        [
+            'nev' => 'Minta Panna',
+            'leiras' => '10 éve foglalkozok gitározással',
+            'irsz' => 1007,
+            'telepules' => 'Budapest',
+            'cim' => 'Petőfi Sándor utca 3',
+            'telefonszam' => 06012345678,
+            'email' => 'teszt@teszt.com',
+            'oradij' => 5000,
+            'elofizetes_lejarata' => '2024-05-11'
+        ],
+        [
+            'nev' => 'Kovács Antal',
+            'leiras' => '10 éve foglalkozok cimbalomozással',
+            'irsz' => 1007,
+            'telepules' => 'Budapest',
+            'cim' => 'Petőfi Sándor utca 5',
+            'telefonszam' => 06012345678,
+            'email' => 'teszt@teszt.com',
+            'oradij' => 5000,
+            'elofizetes_lejarata' => '2024-05-21'
+        ],[
+            'nev' => 'Kovács János',
+            'leiras' => '10 éve foglalkozok kalimbázással',
+            'irsz' => 1007,
+            'telepules' => 'Budapest',
+            'cim' => 'Petőfi Sándor utca 55',
+            'telefonszam' => 06012345678,
+            'email' => 'teszt@teszt.com',
+            'oradij' => 5000,
+            'elofizetes_lejarata' => '2024-05-20'
+        ],
+        [
+            'nev' => 'Kovács Béla',
+            'leiras' => '10 éve foglalkozok zongorázással',
+            'irsz' => 1007,
+            'telepules' => 'Budapest',
+            'cim' => 'Petőfi Sándor utca 2',
+            'telefonszam' => 06012345678,
+            'email' => 'teszt@teszt.com',
+            'oradij' => 5000,
+            'elofizetes_lejarata' => '2024-05-01'
+        ],
+        [
+            'nev' => 'Teszt Tamás',
+            'leiras' => '10 éve foglalkozok dobolással',
+            'irsz' => 1007,
+            'telepules' => 'Budapest',
+            'cim' => 'Petőfi Sándor utca 62',
+            'telefonszam' => 06012345678,
+            'email' => 'teszt@teszt.com',
+            'oradij' => 5000,
+            'elofizetes_lejarata' => '2024-05-25'
+        ],[
+            'nev' => 'Teszt Valaki',
+            'leiras' => '10 éve foglalkozok fuvolával',
+            'irsz' => 1007,
+            'telepules' => 'Budapest',
+            'cim' => 'Petőfi Sándor utca 255',
+            'telefonszam' => 06012345678,
+            'email' => 'teszt@teszt.com',
+            'oradij' => 5000,
+            'elofizetes_lejarata' => '2024-07-21'
+        ]
+        ];
     }
 }
