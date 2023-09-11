f783c931f4ec1d7bc19622673ed6a0faa3e00b7f
unknown <schmidtattila@elg-bp.edu.hu>
2023-09-11 12:24:45 +0200

    Jogkor model

---
+++ app/Models/UserJogkor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserJogkor extends Model
+{
+    use HasFactory;
+    public $table="user_jogkorok";
+    public $timestamps = false;
+    public $primaryKey ="uj_id"
+}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,16 @@
                         </div>
 
                         <div class="row mb-3">
+                        <label for="user_type">Tanárként vagy diákként akarsz regisztrálni?</label>
+                            <div class="col-md-6">
+                                <select name="user_type" id="user_type" class="form-control">
+                                    <option value="">Kérjük válassz!</option>
+                                    <option value="1">Tanár</option>
+                                    <option value="2">Diák</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
