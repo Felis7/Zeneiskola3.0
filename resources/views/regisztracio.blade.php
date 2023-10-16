c475bef180222630c6e83c77dddcf92f42cb11e3
Jászainé Nádudvari Judit <jaszainenadudvarij@EOTVOS2.LOCAL>
2023-10-16 19:45:52 +0200

    suliban

--- resources/views/regisztracio.blade.php
+++ resources/views/regisztracio.blade.php
@@ -2,47 +2,75 @@
 @section('title', "Regisztráció")
 
 @section('content')
-<div class="container">
+
+
+
+<div class="container-fluid">
   <div class="row">
-    <div class="col-4"></div>
-    <div class="col-4">
-      <div class="bg-light p-5 rounded-2">
+    <div class="col-12 col-md-4">
+      <div class="bg-light p-3 rounded">
+        <form method="POST">
+            <label for="kepInput" class="form-label">Profilkép:</label>
+            <input type="image" class="form-control" id="kepInput" name="kepInput" value="{{'old(kepInput')}}">
+            @error('')
+                
+            <button type="submit" class="btn btn-primary mt-5">Csere</button>
+            <button type="submit" class="btn btn-danger mt-5">Törlés</button>
+        </form>     
+      </div>
+    </div>
+    <div class="col-12 col-md-4">
+      <div class="bg-light p-3 rounded">
         <form method="POST">
           <label for="nevInput" class="form-label">Név:</label>
           <input type="text" class="form-control" id="nevInput" name="nevInput">
 
-          <label for="kepInput" class="form-label">Profilkép:</label>
-          <input type="file" class="form-control" id="kepInput" name="kepInput">
+          <label for="leirasInput" class="form-label mt-3">Magadról:</label>        
+          <textarea name="leirasInput" id="leirasInput" cols="30" rows="3" class="form-control"></textarea>        
 
-          <label for="leirasInput" class="form-label mt-3">Magadról:</label>
-          <textarea name="leirasInput" id="leirasInput" cols="30" rows="3" class="form-control"></textarea>
+          <label for="irszamInput" class="form-label mt-3">Irányítószám:</label>        
+          <input type="number" name="irszamInput" id="irszamInput" class="form-control">        
 
-          <label for="irszamInput" class="form-label mt-3">Irányítószám:</label>
-          <input type="number" name="irszamInput" id="irszamInput" class="form-control">
+          <label for="telepulesInput" class="form-label mt-3">Település:</label>        
+          <input type="text" name="telepulesInput" id="telepulesInput" class="form-control">        
 
-          <label for="telepulesInput" class="form-label mt-3">Település:</label>
-          <input type="text" name="telepulesInput" id="telepulesInput" class="form-control">
+          <label for="cimInput" class="form-label mt-3">Cím:</label>        
+          <input type="text" name="cimInput" id="cimInput" class="form-control">        
 
-          <label for="cimInput" class="form-label mt-3">Cím:</label>
-          <input type="text" name="cimInput" id="cimInput" class="form-control">
-
-          <label for="telszamInput" class="form-label mt-3">Telefonszám:</label>
+          <label for="telszamInput" class="form-label mt-3">Telefonszám:</label>        
           <input type="tel" name="telszamInput" id="telszamInput" class="form-control">
+       
 
-          <label for="emailInput" class="form-label mt-3">E-mail:</label>
-          <input type="text" name="emailInput" id="emailInput" class="form-control">
+          <label for="oradijInput" class="form-label mt-3">Óradíj:</label>        
+          <input type="number" name="oradijInput" id="oradijInput" class="form-control">        
 
-          <label for="oradijInput" class="form-label mt-3">Óradíj:</label>
-          <input type="number" name="oradijInput" id="oradijInput" class="form-control">
-
-          <div class="text-center">
-            <button type="submit" class="btn btn-warning mt-5">Regisztrálás</button>
-          </div>
-        </form>
+          <button type="submit" class="btn btn-warning mt-5 mb-3">Mentés</button>                
+        </form>        
       </div>
     </div>
-    <div class="col-4"></div>
+    <div class="col-12 col-md-4">
+      <div class="bg-light p-3 rounded">
+        <form method="POST">
+          <label for="emailInput" class="form-label mt-3">E-mail:</label>        
+          <input type="email" name="emailInput" id="emailInput" class="form-control">        
+
+          <label for="ujJelszoInput"class="form-label mt-3">Új jelszó:</label>        
+          <input type="password" name="ujJelszoInput" id="ujJelszoInput" class="form-control">        
+
+          <label for="ujJelszoIsmInput"class="form-label mt-3">Új jelszó megismétlése:</label>        
+          <input type="password" name="ujJelszoIsmInput" id="ujJelszoIsmInput" class="form-control">        
+
+          <label for="jelenlegiJelszoInput"class="form-label mt-3">Jelenlegi jelszó:</label>        
+          <input type="password" name="JelszoIsmInput" id="ujJelszoIsmInput" class="form-control">        
+
+          <button type="submit" class="btn btn-warning mt-5">Mentés</button>        
+        </form>          
+      </div>
+    </div>
   </div>
 </div>
+
+
+
   
 @endsection
