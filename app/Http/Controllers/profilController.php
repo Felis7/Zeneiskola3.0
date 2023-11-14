4345fca62af3f6c90890ca87521282510db2f861
jnjudit <jnjudit79@gmail.com>
2023-11-14 19:10:40 +0100

    profilController.php validálásának befejezése

--- app/Http/Controllers/profilController.php
+++ app/Http/Controllers/profilController.php
@@ -22,14 +22,37 @@ class profilController extends Controller
     public function adatMentes(Request $request){
         $request->validate([
             "nevInput"=>["required"],
-            //folytasd a beviteli mezők alapján
+            "leirasInput"=>["required","min:20"],
+            "irszamInput"=>["required", "numeric", "min:1000", "max:9999",],
+            "telepulesInput"=>["required"],
+            "cimInput"=>["required"],
+            "telszamInput"=>["required", "numeric"],
+            "oradijInput"=>["required", "numeric"]            
         ],[
-            "nevInput.required"=>"A mező kitöltése kötelező",
+            "nevInput.required"=>"A mező kitöltése kötelező!",
+            "leirasInput.required"=>"A mező kitöltése kötelező!",
+            "leirasInput.min"=> "Minimum 20 karakter!",
+            "irszamInput.required"=>"A mező kitöltése kötelező!",
+            "irszamInput.numeric"=>"Csak szám lehet!",
+            "irszamInput.min"=>"Minimum 1000 lehet!",
+            "irszamInput.max"=>"Maximum 9999 lehet!",
+            "telepulesInput.required"=>"A mező kitöltése kötelező!",
+            "cimInput.required"=>"A mező kitöltése kötelező!",
+            "telszamInput"=> "A mező kitöltése kötelező!",
+            "telszamInput.numeric"=>"Csak szám lehet!",
+            "oradijInput.required"=>"A mező kitöltése kötelező!",
+            "oradijInput.numeric"=>"Csak szám lehet!",
         ]);
-        $user = Tanar:: find(Auth::user()->tid);
-        $user->nev= $request->input("nevInput");
-        //folytasd a tábla alapján
-        $user->save();
+        $tanar = Tanar:: find(Auth::user()->tid);
+        $tanar->nev= $request->input("nevInput");
+        $tanar->leiras= $request->input("leirasInput");
+        $tanar->irsz= $request->input("irszamInput");
+        $tanar->telepules= $request->input("telepulesInput");
+        $tanar->cim= $request->input("cimInput");
+        $tanar->telefonszam= $request->input("telszamInput");
+        $tanar->oradij= $request->input("oradijInput");
+
+        $tanar->save();
         return redirect("profil");
     }   
 }
